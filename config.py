95b792f9167678414ead324cd1e77101e17bbc76
sguzman06 <sguzman.0601@gmail.com>
2025-11-26 12:22:49 -0300

    cambio de URL en config.py http://localhost:5678/webhook-test/prueba --> http://localhost:5678/webhook/prueba

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-N8N_WEBHOOK_URL = os.getenv("N8N_WEBHOOK_URL", "http://localhost:5678/webhook-test/prueba")
+N8N_WEBHOOK_URL = os.getenv("N8N_WEBHOOK_URL", "http://localhost:5678/webhook/prueba")
 API_KEY = os.getenv("API_KEY", "")
 TIMEOUT = float(os.getenv("TIMEOUT", "500.0"))
 REPORTS_DIR = os.getenv("REPORTS_DIR", "./reports")
